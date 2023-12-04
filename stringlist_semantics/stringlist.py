30b1a09fee870e675934586342dc8a28924113da
sonalikapatel84 <36246859+sonalikapatel84@users.noreply.github.com>
2023-12-04 14:36:00 +1100

    PR #7 integrate openai Added a method which takes a list of strings as input Returns the tv information

--- stringlist_semantics/stringlist.py
+++ stringlist_semantics/stringlist.py
@@ -1,9 +1,5 @@
 import openai
-import re
-import os
-import config
 import json
-
 
 def chat_completion(user_message):
     response = openai.chat.completions.create(
@@ -12,15 +8,19 @@ def chat_completion(user_message):
     )
     return response.choices[0].message.content
 
-def main():
+def describe_tv(tv_info):
     user_message = {
         "role": "user",
         "content": 
         f'You are an expert in TV selling. Based on the below TV information, retrieve the two strings, '
         f'one for the brand name and the other for the model name of the TV\n###\n '
         f'{json.dumps([{"6_1.jpg": ["8","MSUNG","Hali","ONd}","CerbifiedUHL","UHDTV","CG","CLASS","TNu710O ","7SERIES"]}])}\n###\nBrand:\nModel:'
-     } 
-    chat_completion(user_message)
+    }
+    return chat_completion(user_message)
+
+def main():
+    result = describe_tv(["8","MSUNG","Hali","ONd}","CerbifiedUHL","UHDTV","CG","CLASS","TNu710O ","7SERIES"])
+    print(result)
 
 if __name__ == "__main__":
     main()
