3323d94b1fab1e41e12ffdfaaaa0eeb0761c250d
sonalikapatel84 <36246859+sonalikapatel84@users.noreply.github.com>
2023-12-06 10:22:33 +1100

    CSRAssist PR#7 Added 2 more TV information examples Replaced hard coded string with the variable name

--- stringlist_semantics/stringlist.py
+++ stringlist_semantics/stringlist.py
@@ -28,13 +28,17 @@ def describe_tv(tv_info):
         "content": 
         f'You are an expert in TV selling. Based on the below TV information, retrieve the two strings, '
         f'one for the brand name and the other for the model name of the TV\n###\n '
-        f'{json.dumps([{"6_1.jpg": ["8","MSUNG","Hali","ONd}","CerbifiedUHL","UHDTV","CG","CLASS","TNu710O ","7SERIES"]}])}\n###\nBrand:\nModel:'
+        f'{json.dumps(tv_info)}\n###\nBrand:\nModel:'
     }
     return chat_completion(user_message)
 
 def main():
     # Extract brand and model information from the TV info
     result = describe_tv(["8","MSUNG","Hali","ONd}","CerbifiedUHL","UHDTV","CG","CLASS","TNu710O ","7SERIES"])
+    print(result)
+    result = describe_tv(["SONTY","Meo-","VIAFV8","ANOS","SONY","UHDTV","N4","Moeloclp", "W90", "46", "Brasil ","mdke.","beleve", "BRAVIA", "Feel", "the", "Beauty"])
+    print(result)
+    result = describe_tv(["Kogan", "24", "inches", "4a", "and", "roid", "RH", "9310"])
     print(result)
 
 if __name__ == "__main__":
